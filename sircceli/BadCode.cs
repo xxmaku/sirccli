774993a11129e92e2c9a3f7bbbeb0de12426cf16
xxmaku <186540982+xxmaku@users.noreply.github.com>
2026-03-09 16:32:26 +0200

    Add BadCode class with deeply nested loops for demonstration purposes

---
+++ sircceli/BadCode.cs
@@ -0,0 +1,131 @@
+namespace sircceli;
+
+public class BadCode
+{
+    public void Foo()
+    {
+        if (true)
+        {
+            if (true)
+            {
+                if (true)
+                {
+                    if (true)
+                    {
+                        if (true)
+                        {
+                            if (true)
+                            {
+                                if (true)
+                                {
+                                    for (int i = 0; i < 10; i++)
+                                    {
+                                        if (true)
+                                        {
+                                            for (int o = 0; o < 10; o++)
+                                            {
+                                                if (true)
+                                                {
+                                                    for (int p = 0; p < 10; p++)
+                                                    {
+                                                        if (true)
+                                                        {
+                                                            for (int q = 0; q < 10; q++)
+                                                            {
+                                                                if (true)
+                                                                {
+                                                                    for (int r = 0; r < 10; r++)
+                                                                    {
+                                                                        if (true)
+                                                                        {
+                                                                            for (int s = 0; s < 10; s++)
+                                                                            {
+                                                                                if (true)
+                                                                                {
+                                                                                    for (int t = 0; t < 10; t++)
+                                                                                    {
+                                                                                        if (true)
+                                                                                        {
+                                                                                            for (int u = 0; u < 10; u++)
+                                                                                            {
+                                                                                                if (true)
+                                                                                                {
+                                                                                                    for (int v = 0;
+                                                                                                         v < 10;
+                                                                                                         v++)
+                                                                                                    {
+                                                                                                        if (true)
+                                                                                                        {
+                                                                                                            for (int w =
+                                                                                                                     0;
+                                                                                                                 w < 10;
+                                                                                                                 w++)
+                                                                                                            {
+                                                                                                                if
+                                                                                                                    (true)
+                                                                                                                {
+                                                                                                                    for
+                                                                                                                        (int
+                                                                                                                         x =
+                                                                                                                             0;
+                                                                                                                         x <
+                                                                                                                         10;
+                                                                                                                         x++)
+                                                                                                                    {
+                                                                                                                        if
+                                                                                                                            (true)
+                                                                                                                        {
+                                                                                                                            for
+                                                                                                                                (int
+                                                                                                                                 y =
+                                                                                                                                     0;
+                                                                                                                                 y <
+                                                                                                                                 10;
+                                                                                                                                 y++)
+                                                                                                                            {
+                                                                                                                                if
+                                                                                                                                    (true)
+                                                                                                                                {
+                                                                                                                                    for
+                                                                                                                                        (int
+                                                                                                                                         z =
+                                                                                                                                             0;
+                                                                                                                                         z <
+                                                                                                                                         10;
+                                                                                                                                         z++)
+                                                                                                                                    {
+                                                                                                                                        // Do something
+                                                                                                                                    }
+                                                                                                                                }
+                                                                                                                            }
+                                                                                                                        }
+                                                                                                                    }
+                                                                                                                }
+                                                                                                            }
+                                                                                                        }
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
