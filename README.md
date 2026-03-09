729bff61005d735a7a6c816a21c68d7cee2eabfc
xxmaku <186540982+xxmaku@users.noreply.github.com>
2026-03-09 09:05:57 +0200

    Rename project, add sample UI code

---
+++ README.md
@@ -0,0 +1,2 @@
+# SIRCCELI (SEPRO ÄM IRK KLIENT)
+*TODO: Write about the project here*
---
+++ sircceli/Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Hosting;
+using RazorConsole.Core;
+
+var builder = Host.CreateDefaultBuilder(args)
+    .UseRazorConsole<sircceli.UI.Counter>();
+
+var host = builder.Build();
+
+await host.RunAsync();
