877e63304fa881063d3eccc84d2634d39a80a23b
xxmaku <186540982+xxmaku@users.noreply.github.com>
2026-03-14 16:48:51 +0200

    Add unit tests for userlist view

---
+++ sircceli.test/UserListTest.cs
@@ -0,0 +1,21 @@
+﻿using Bunit;
+using Xunit;
+using sircceli.UI;
+
+public class UserListTest
+{
+    [Fact]
+    public void UserListDisplaysUsers()
+    {
+        using var ctx = new TestContext();
+
+        // Arrange & Act
+        var cut = ctx.Render<UserList>();
+
+        // Assert
+        
+        Assert.Contains("user1", cut.Markup);
+        Assert.Contains("user2", cut.Markup);
+        Assert.Contains("user3", cut.Markup);
+    }
+}
