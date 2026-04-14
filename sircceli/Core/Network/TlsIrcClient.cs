292c307bbc37ae495c569edd6f98ef2aa2a88f48
xxmaku <186540982+xxmaku@users.noreply.github.com>
2026-04-14 10:32:58 +0300

    Rename, use interface as base

---
+++ sircceli/Core/Network/TlsIrcClient.cs
@@ -0,0 +1,26 @@
+using sircceli.Models;
+
+namespace sircceli.Core.Network;
+
+public class TlsIrcClient : IIrcClient, IDisposable
+{
+    public bool IsConnected { get; }
+    public IReadOnlyList<string> CurrentUsers { get; }
+    public event EventHandler<bool>? ConnectionStateChanged;
+    public event EventHandler<IReadOnlyList<string>>? ChannelUsersChanged;
+    public event EventHandler<Message>? MessageReceived;
+    void IIrcClient.Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SendMessage(string message)
+    {
+        throw new NotImplementedException();
+    }
+
+    void IDisposable.Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}
